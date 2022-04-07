2543bdb2fbf42d80c0c5e112abed75c1e3ed07f1
Bryce Lorenz Kille <brycekille@gmail.com>
2022-04-07 14:24:54 -0500

    Update copy

--- include/bitlib/bit-algorithms/copy.hpp
+++ include/bitlib/bit-algorithms/copy.hpp
@@ -14,7 +14,9 @@
 
 // ================================ PREAMBLE ================================ //
 // C++ standard library
+#include <type_traits>
 // Project sources
+#include "bitlib/bitlib.hpp"
 // Third-party libraries
 // Miscellaneous
 namespace bit {
@@ -24,102 +26,80 @@ namespace bit {
 
 // ---------------------------- Copy Algorithms ----------------------------- //
 
-// Status: Does not work for Input/Output it due to distance call
-template <class InputIt, class OutputIt>
-constexpr bit_iterator<OutputIt> copy(bit_iterator<InputIt> first,
-                            bit_iterator<InputIt> last,
-                            bit_iterator<OutputIt> d_first
+// Status: Does not work for Input/Output iterators due to distance call
+template <class RandomAccessIt1, class RandomAccessIt2>
+constexpr bit_iterator<RandomAccessIt2> copy(bit_iterator<RandomAccessIt1> first,
+                            bit_iterator<RandomAccessIt1> last,
+                            bit_iterator<RandomAccessIt2> d_first
 )
 {
+    // Types and constants
+    using dst_word_type = typename bit_iterator<RandomAccessIt2>::word_type;
+    using src_word_type = typename bit_iterator<RandomAccessIt1>::word_type;
+    using word_type = dst_word_type;
+    using size_type = typename bit_iterator<RandomAccessIt2>::size_type;
+    constexpr size_type digits = binary_digits<word_type>::value;
+
     // Assertions
     _assert_range_viability(first, last);
+    static_assert(std::is_same<dst_word_type, src_word_type>::value, "Underlying word types must be equal");
     if (first == last) return d_first;
 
-    // Types and constants
-    using dst_word_type = typename bit_iterator<OutputIt>::word_type;
-    using src_word_type = typename bit_iterator<InputIt>::word_type;
-    using dst_size_type = typename bit_iterator<OutputIt>::size_type;
-    constexpr dst_size_type dst_digits = binary_digits<dst_word_type>::value;
 
     // Initialization
     const bool is_d_first_aligned = d_first.position() == 0;
-    dst_size_type total_bits_to_copy = distance(first, last);
+    size_type total_bits_to_copy = distance(first, last);
+    size_type remaining_bits_to_copy = total_bits_to_copy;
     auto it = d_first.base();
+
 
     // d_first is not aligned. Copy partial word to align it
     if (!is_d_first_aligned) {
-        dst_size_type partial_bits_to_copy = std::min(
-                total_bits_to_copy,
-                dst_digits - d_first.position()
+        size_type partial_bits_to_copy = std::min(
+                remaining_bits_to_copy,
+                digits - d_first.position()
                 );
         *it = _bitblend(
                 *it,
-                static_cast<dst_word_type>(
-                  get_word<dst_word_type>(first, partial_bits_to_copy) 
-                    << static_cast<dst_word_type>(d_first.position())
+                static_cast<word_type>(
+                  get_word<word_type>(first, partial_bits_to_copy) 
+                    << static_cast<word_type>(d_first.position())
                 ),
-                static_cast<dst_word_type>(d_first.position()),
-                static_cast<dst_word_type>(partial_bits_to_copy)
+                static_cast<word_type>(d_first.position()),
+                static_cast<word_type>(partial_bits_to_copy)
                 );
-        total_bits_to_copy -= partial_bits_to_copy;
+        remaining_bits_to_copy -= partial_bits_to_copy;
         std::advance(first, partial_bits_to_copy);
         it++;
     }
 
-    const bool is_first_aligned = first.position() == 0;
-    // d_first will be aligned at this point
-    if (is_first_aligned && std::is_same<dst_word_type, src_word_type>()) {
-        auto N = std::distance(first.base(), last.base());
-        it = std::copy(first.base(), last.base(), it);
-        first += dst_digits * N;
-        total_bits_to_copy -= dst_digits * N;
-    } else {
-        while (total_bits_to_copy >= dst_digits) {
-            *it = get_word<dst_word_type>(first, dst_digits);
-            total_bits_to_copy -= dst_digits;
-            it++; 
-            std::advance(first, dst_digits);
+    if (remaining_bits_to_copy > 0) { 
+        const bool is_first_aligned = first.position() == 0;
+        // d_first will be aligned at this point
+        if (is_first_aligned) {
+            auto N = std::distance(first.base(), last.base());
+            it = std::copy(first.base(), last.base(), it);
+            first += digits * N;
+            remaining_bits_to_copy -= digits * N;
+        } else {
+            while (remaining_bits_to_copy >= digits) {
+                *it = get_word<word_type>(first, digits);
+                remaining_bits_to_copy -= digits;
+                it++; 
+                std::advance(first, digits);
+            }
+        }
+        if (remaining_bits_to_copy > 0) {
+            *it = _bitblend(
+                    *it,
+                    get_word<word_type>(first, remaining_bits_to_copy),
+                    static_cast<word_type>(
+                        (static_cast<word_type>(1) << remaining_bits_to_copy) - 1)
+            );
         }
     }
-    if (total_bits_to_copy) {
-        *it = _bitblend(
-                *it,
-                get_word<dst_word_type>(first, total_bits_to_copy),
-                static_cast<dst_word_type>(
-                    (static_cast<dst_word_type>(1) << total_bits_to_copy) - 1)
-        );
-    }
-    return bit::bit_iterator<OutputIt>(it, total_bits_to_copy);
+    return d_first + total_bits_to_copy;
 }
-
-// Status: on hold
-template <class ExecutionPolicy, class ForwardIt1, class ForwardIt2>
-bit_iterator<ForwardIt2> copy(ExecutionPolicy&& policy, 
-    bit_iterator<ForwardIt1> first, bit_iterator<ForwardIt1> last,
-    bit_iterator<ForwardIt2> d_first) {
-    (policy, first, last);    
-    return d_first;
-}
-
-// Status: on hold
-template <class InputIt, class OutputIt, class UnaryPredicate>
-constexpr bit_iterator<OutputIt> copy_if(bit_iterator<InputIt> first,
-    bit_iterator<InputIt> last, bit_iterator<OutputIt> d_first,
-    UnaryPredicate pred) {
-    (first, last, pred);
-    return d_first;
-}
-
-// Status: on hold
-template <class ExecutionPolicy, class ForwardIt1, class ForwardIt2,
-    class UnaryPredicate> bit_iterator<ForwardIt2> copy_if(ExecutionPolicy&& policy,
-    bit_iterator<ForwardIt1> first, bit_iterator<ForwardIt1> last, 
-    bit_iterator<ForwardIt2> d_first, UnaryPredicate pred) {
-    (policy, first, last, pred);
-    return d_first; 
-}
-
-
 // -------------------------------------------------------------------------- //
 
 
