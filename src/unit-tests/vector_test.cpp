a03258fd81282798790cfe9f00402696f6daffe0
Bryce Lorenz Kille <brycekille@gmail.com>
2022-05-01 09:27:18 -0500

    constructor tests

--- src/unit-tests/vector_test.cpp
+++ src/unit-tests/vector_test.cpp
@@ -9,6 +9,7 @@
 // ============================== PREAMBLE ================================== //
 // C++ standard library
 #include <math.h>
+#include <type_traits>
 // Project sources
 #include "bitlib/bit-containers/bit-containers.hpp"
 #include "fixtures.hpp"
@@ -28,6 +29,25 @@ TYPED_TEST(VectorTest, DefaultConstructor) {
 // Tests the size c'tor.
 TYPED_TEST(VectorTest, SizeInitializerConstructor) {
     EXPECT_EQ(18, this->v2_.size());
+    for (auto bv: this->v2_) {
+        EXPECT_FALSE(bv);
+    }
+    // hack to get the word type, since for some reason I'm not allowed to use this->base_type
+    using base_type = typename std::remove_reference<decltype(*(this->empty_vec.begin().base()))>::type;
+    for (unsigned int veclen = 0; veclen < 4*this->digits; veclen++) {
+        this->empty_vec = decltype(this->empty_vec)(veclen);
+        for (unsigned int i = 0; i < veclen; ++i) {
+            EXPECT_FALSE(this->empty_vec[i]);
+        }
+        this->empty_vec = bit::bit_vector<base_type>(veclen);
+        unsigned int i = 0;
+        // TODO misplaced test for range-based for loop
+        for (auto bv: this->empty_vec) {
+            EXPECT_FALSE(bv);
+            i++; 
+        }
+        EXPECT_EQ(i, veclen);
+    }
 }
 
 // Tests the string c'tor.
@@ -35,6 +55,22 @@ TYPED_TEST(VectorTest, StringConstructor) {
     EXPECT_EQ(9, this->v3_.size());
     EXPECT_EQ(false, static_cast<bool>(this->v3_[0]));
     EXPECT_EQ(true, static_cast<bool>(this->v3_[8]));
+    using base_type = typename std::remove_reference<decltype(*(this->empty_vec.begin().base()))>::type;
+    for (unsigned int strlen = 0; strlen < 4*this->digits; strlen++) {
+        std::string rand_bs(strlen, 0);
+        this->empty_vec_bool.clear();
+        for (auto& pos: rand_bs) {
+            pos = generate_random_number('0', '1');
+            this->empty_vec_bool.push_back(pos == '1');
+        }
+        this->empty_vec = bit::bit_vector<base_type>(rand_bs);
+        EXPECT_TRUE(std::equal(
+                    this->empty_vec.begin(), 
+                    this->empty_vec.end(), 
+                    this->empty_vec_bool.begin(), 
+                    this->empty_vec_bool.end(), 
+                    comparator));
+    }
 }
 
 // Test the copy c'tor
@@ -48,6 +84,22 @@ TYPED_TEST(VectorTest, CopyConstructor) {
         this->v2_.insert(this->v2_.end(), bit::bit0);
         this->v2_.insert(this->v2_.end(), bit::bit1);
     }
+    for (unsigned int vec_idx = 0; vec_idx < this->random_bitvecs.size(); ++vec_idx) {
+        auto& bitvec = this->random_bitvecs[vec_idx];
+        auto& boolvec = this->random_boolvecs[vec_idx];
+        this->empty_vec = bitvec;
+        EXPECT_NE(this->empty_vec.begin().base(), bitvec.begin().base());
+        EXPECT_TRUE(std::equal(
+                    this->empty_vec.begin(), 
+                    this->empty_vec.end(), 
+                    boolvec.begin(), 
+                    boolvec.end(), 
+                    comparator));
+        if (bitvec.size() > 0) {
+            bitvec[0] = ~bitvec[0];
+            EXPECT_NE(bitvec[0], this->empty_vec[0]);
+        }
+    }
 }
 
 // Test the move c'tor
@@ -58,6 +110,19 @@ TYPED_TEST(VectorTest, MoveConstructor) {
     EXPECT_EQ(this->empty_vec.begin().base(), v2_data);
     EXPECT_TRUE(std::equal(this->empty_vec.begin(), this->empty_vec.end(), v2_copy.begin()));
     EXPECT_EQ(this->empty_vec.size(), v2_copy.size());
+    for (unsigned int vec_idx = 0; vec_idx < this->random_bitvecs.size(); ++vec_idx) {
+        auto& bitvec = this->random_bitvecs[vec_idx];
+        auto& boolvec = this->random_boolvecs[vec_idx];
+        auto bitvec_base = bitvec.begin().base();
+        this->empty_vec = std::move(bitvec);
+        EXPECT_EQ(this->empty_vec.begin().base(), bitvec_base);
+        EXPECT_TRUE(std::equal(
+                    this->empty_vec.begin(), 
+                    this->empty_vec.end(), 
+                    boolvec.begin(), 
+                    boolvec.end(), 
+                    comparator));
+    }
 }
 
 
@@ -496,6 +561,47 @@ TYPED_TEST(VectorTest, EraseAtRand2) {
                 std::distance(boolvec.begin(), boolret));
     }
 }
+
+// Test push_back
+TYPED_TEST(VectorTest, PushBack) {
+    // First signature
+    for (unsigned int vec_idx = 0; vec_idx < this->random_bitvecs.size(); ++vec_idx) {
+        for (auto _ = 4*this->digits; _ > 0; _--) {
+            auto& bitvec = this->random_bitvecs[vec_idx];
+            auto& boolvec = this->random_boolvecs[vec_idx];
+            bool to_insert_bool = generate_random_number(0, 1) > 0 ? true : false;
+            bit::bit_value to_insert_bit = to_insert_bool ? bit::bit1 : bit::bit0;
+            bitvec.push_back(to_insert_bit);
+            boolvec.push_back(to_insert_bool);
+            EXPECT_TRUE(std::equal(
+                        bitvec.begin(), 
+                        bitvec.end(), 
+                        boolvec.begin(), 
+                        boolvec.end(), 
+                        comparator));
+        }
+    }
+}
+
+// Test pop_back
+TYPED_TEST(VectorTest, PopBack) {
+    // First signature
+    for (unsigned int vec_idx = 0; vec_idx < this->random_bitvecs.size(); ++vec_idx) {
+        auto& bitvec = this->random_bitvecs[vec_idx];
+        auto& boolvec = this->random_boolvecs[vec_idx];
+        for (auto _ = std::min(boolvec.size(), 4*this->digits); _>0; _--) {
+            bitvec.pop_back();
+            boolvec.pop_back();
+            EXPECT_TRUE(std::equal(
+                        bitvec.begin(), 
+                        bitvec.end(), 
+                        boolvec.begin(), 
+                        boolvec.end(), 
+                        comparator));
+        }
+    }
+}
+
 
 //TYPED_TEST(VectorTest, Print) {
     //std::cout << this->v3_ << std::endl;
