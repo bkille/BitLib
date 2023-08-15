2eca106537118034b65adf98a46236e7e24614d4
Bryce Lorenz Kille <brycekille@gmail.com>
2023-08-15 16:36:46 -0500

    Add simd for count (honestly not even faster though)

--- include/bitlib/bit-algorithms/count.hpp
+++ include/bitlib/bit-algorithms/count.hpp
@@ -14,12 +14,19 @@
 // Project sources
 #include "bitlib/bit-iterator/bit.hpp"
 // Third-party libraries
+#ifdef BITLIB_HWY
+#include "hwy/highway.h"
+#endif
 // Miscellaneous
 
 #define is_aligned(POINTER, BYTE_COUNT) \
     (((uintptr_t)(const void *)(POINTER)) % (BYTE_COUNT) == 0)
 
 namespace bit {
+
+#ifdef BITLIB_HWY
+namespace hn = hwy::HWY_NAMESPACE;
+#endif
 // ========================================================================== //
 
 template<class RandomAccessIt>
@@ -49,16 +56,38 @@ count(
             result = _popcnt(first_value);
             ++it;
         }
-        for (; it != last.base() && !is_aligned(&(*it), 64); ++it) {
-            result += _popcnt(*it);
+#ifdef BITLIB_HWY
+        // ReduceSum not implemented for unsigned char
+        if constexpr (digits > 8)
+        {
+            // Align to boundary
+            for (; it != last.base() && !is_aligned(&(*it), 64); ++it) {
+                result += _popcnt(*it);
+            }
+
+            // SIMD
+            hn::ScalableTag<word_type> d;
+            for (; std::distance(it, last.base()) >= hn::Lanes(d); it += hn::Lanes(d))
+            {
+                const auto popcntV = hn::PopulationCount(hn::Load(d, &*it));
+                result += hn::ReduceSum(d, popcntV);
+            }
+
+            // Remaining 
+            for (; it != last.base(); ++it) {
+                result += _popcnt(*it);
+            }
+        } else
+#endif
+        {
+            result += std::transform_reduce(
+                    it, 
+                    last.base(), 
+                    0, 
+                    std::plus{}, 
+                    [](word_type word) {return _popcnt(word); }
+            );
         }
-        result += std::transform_reduce(
-                it, 
-                last.base(), 
-                0, 
-                std::plus{}, 
-                [](word_type word) {return _popcnt(word); }
-        );
         if (last.position() != 0) {
             word_type last_value = *last.base() << (digits - last.position());
             result += _popcnt(last_value);
